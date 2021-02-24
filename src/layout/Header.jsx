4b3186e99cd4639cb3ae6e89a6f13d9af93844aa
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2021-02-24 17:04:36 +0300

    project complete

---
+++ src/layout/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+	return (
+		<header>
+			<nav className="purple darken-1">
+				<div className="nav-wrapper">
+					<a href="#" className="brand-logo">Films</a>
+					<ul id="nav-mobile" className="right hide-on-med-and-down">
+						<li><a href="collapsible.html" href="!#">report</a></li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+	);
+}
+
+export {Header}
